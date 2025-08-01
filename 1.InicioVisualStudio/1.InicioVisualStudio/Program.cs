cc7654226b674b8de98da762c189808d671349aa
Juan Camilo E <juanc.escobarv@upb.edu.co>
2025-08-01 09:31:59 -0500

    Variables, tipos de datos y constantes

--- 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
+++ 1.InicioVisualStudio/1.InicioVisualStudio/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declaracion e inicializacion de variables
+            string nombre="Juan C";
+            const string iva = "19%";
+            string apellidos = "Escobar Vanegas";
+            Console.Write(nombre+" Bienvenido al curso de fundamentos de programacion\n");
+            nombre = "Camilo";
+            //iva = "20%"; NO puedo cambiar el valor de iva por ser una constante
+            Console.Write("\t" + nombre + " " +apellidos + " " + iva);
+            Console.ReadKey();
         }
     }
 }
